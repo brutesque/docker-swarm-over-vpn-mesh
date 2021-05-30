8b97867de0510954d382eadf6593df5f118cdf03
Brutesque <brutesque@users.noreply.github.com>
2021-05-30 06:19:09 +0200

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -116,6 +116,7 @@ variable "vultr_token" {
   sensitive   = true
   default     = null
 }
+
 variable "manager_count" {
   type        = number
   description = "Number of manager nodes in the docker swarm"
