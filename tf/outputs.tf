64bb8739a0043e226f966fdf1fcd8f87de277ddc
Brutesque <brutesque@users.noreply.github.com>
2021-05-30 06:05:39 +0200

    readability

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -11,10 +11,10 @@ locals {
           local.vultr_instances,
   )
 
-  docker_managers = slice(keys(local.all_instances), 0, min(2, length(local.all_instances)))
-  docker_workers = slice(keys(local.all_instances), min(2, length(local.all_instances)), length(local.all_instances))
-  entrypoints = slice(keys(local.all_instances), 0, min(5, length(local.all_instances)))
-  glusterpool = keys(local.all_instances)
+  docker_managers = slice(keys(local.all_instances), 0, min(var.manager_count, length(local.all_instances)))
+  docker_workers  = slice(keys(local.all_instances), min(var.manager_count, length(local.all_instances)), length(local.all_instances))
+  entrypoints     = slice(keys(local.all_instances), 0, min(5, length(local.all_instances)))
+  glusterpool     = keys(local.all_instances)
   duckdns_subdomains = var.duckdns_subdomains
 }
 
@@ -22,12 +22,12 @@ locals {
 resource "local_file" "AnsibleInventory" {
   content = templatefile("ansible-inventory.tmpl",
     {
-      all-instances = local.all_instances,
-      prem-instances = keys(local.prem_instances),
-      docker-managers = local.docker_managers,
-      docker-workers = local.docker_workers,
-      entrypoints = local.entrypoints,
-      glusterpool = local.glusterpool,
+      all-instances      = local.all_instances,
+      prem-instances     = keys(local.prem_instances),
+      docker-managers    = local.docker_managers,
+      docker-workers     = local.docker_workers,
+      entrypoints        = local.entrypoints,
+      glusterpool        = local.glusterpool,
       duckdns-subdomains = local.duckdns_subdomains,
     }
   )
@@ -38,17 +38,17 @@ resource "local_file" "AnsibleInventory" {
 resource "local_file" "AnsibleVars" {
   content = templatefile("ansible-vars.tmpl",
     {
-      duckdns_token = var.duckdns_token,
-      ssh_public_key = file(var.ssh_public_key_path),
-      domain_name = var.domain_name,
-      admin_password = var.admin_password,
-      admin_email = var.admin_email,
-      user_name = var.user_name,
-      user_password = var.user_password,
+      duckdns_token    = var.duckdns_token,
+      ssh_public_key   = file(var.ssh_public_key_path),
+      domain_name      = var.domain_name,
+      admin_password   = var.admin_password,
+      admin_email      = var.admin_email,
+      user_name        = var.user_name,
+      user_password    = var.user_password,
       stacks_portainer = var.stacks_portainer,
-      stacks_swarmpit = var.stacks_swarmpit,
+      stacks_swarmpit  = var.stacks_swarmpit,
       stacks_swarmprom = var.stacks_swarmprom,
-      stacks_tests = var.stacks_tests,
+      stacks_tests     = var.stacks_tests,
     }
   )
   filename = "../secrets/vars.yml"
